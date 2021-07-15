ccd382a6a8107e14299f66a300eb5850aab706b8
lalebdi <loai.alebdi@gmail.com>
2021-07-14 21:14:29 -0400

    review

---
+++ matrixes.py
@@ -0,0 +1,14 @@
+size = int(input())
+
+array_input = []
+
+for x in range(size):
+    array_input.append([int(y) for y in input().split()])
+
+print(array_input)
+
+
+for i in array_input:
+    for j in i:
+        print(j, end=" ")
+    print()
