6e8c2492e584f2f79657741bda1b0834006764e9
lalebdi <loai.alebdi@gmail.com>
2021-07-19 13:39:58 -0400

    init

---
+++ arrays.py
@@ -0,0 +1,34 @@
+from array import *
+
+# Create array and tranverse
+
+# Access indiviual elements through indexes
+
+# Append any value to the array
+
+# Insert value in an array
+
+# Extend array
+
+
+# Add items from list into array
+
+# Remove any array element
+
+# Remove last array element
+
+# Fetch any element
+
+# Reverse an array
+
+# Get array buffer information
+
+# Check for number of occurences of an element using count
+
+# Convert array to string
+
+# Convert array to a list
+
+# Append a string to char array
+
+# Slice elements from an array
