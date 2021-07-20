a72f008917f26653bdf442694a7a2ce3bb4ed16c
lalebdi <loai.alebdi@gmail.com>
2021-07-20 11:49:10 -0400

    debug

--- SinglyLinkedLists.py
+++ SinglyLinkedLists.py
@@ -16,30 +16,34 @@ class SLinkedList:
             node = node.next
 
     def insertSSL(self, value, location):
-        newNode = Node()
+        newNode = Node(value)
         if self.head is None:
             self.head = newNode
             self.tail = newNode
-        else::
-        if location == 0: # inserting to the beginning of the linked list
-            newNode.next = self.head
-            self.head = newNode
-        elif location == 1: # inserting to the end of the linked list
-            newNode.next = Node()
-            self.tail.next = newNode
-            self.tail = newNode
-        else: # inserting to in the middle
-            tempNode = self.head
-            index = 0
-            while index < location - 1:
-                tempNode = tempNode.next
-                index += 1
-            nextNode = tempNode.next
-            tempNode.next = newNode
-            newNode.next = nextNode
+        else:
+            if location == 0: # inserting to the beginning of the linked list
+                newNode.next = self.head
+                self.head = newNode
+            elif location == 1: # inserting to the end of the linked list
+                newNode.next = None
+                self.tail.next = newNode
+                self.tail = newNode
+            else: # inserting to in the middle
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode
 
 
 
 
 singlyLinkedList = SLinkedList()
+singlyLinkedList.insertSSL(1, 1)
+singlyLinkedList.insertSSL(2, 1)
+singlyLinkedList.insertSSL(3, 1)
+singlyLinkedList.insertSSL(4, 1)
 print([node.value for node in singlyLinkedList])
