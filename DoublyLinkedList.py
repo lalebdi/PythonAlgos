ac7667e9ca03851f9cabb84e737396db6638a94a
lalebdi <loai.alebdi@gmail.com>
2021-07-20 12:01:14 -0400

    init

---
+++ DoublyLinkedList.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
