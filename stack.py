6ae44db15018edfc636737b0ca3cdd36c8406d95
lalebdi <loai.alebdi@gmail.com>
2021-07-20 12:23:52 -0400

    isEmpty

--- stack.py
+++ stack.py
@@ -3,10 +3,21 @@ class Node:
         self.value = value
         self.next = None
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
 
+
 class Stack:
     def __init__(self):
         self.LinkedList = LinkedList()
+
+    def isEmpty(self):
+        if self.LinkedList.head == None:
+            return True
+        return False
+
+
+customStack = Stack()
+print(customStack.isEmpty())
