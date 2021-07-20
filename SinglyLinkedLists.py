9b4d753529cba5a9025e274e2172e70fc4628b77
lalebdi <loai.alebdi@gmail.com>
2021-07-20 11:45:29 -0400

    insert

--- SinglyLinkedLists.py
+++ SinglyLinkedLists.py
@@ -15,6 +15,30 @@ class SLinkedList:
             yield node
             node = node.next
 
+    def insertSSL(self, value, location):
+        newNode = Node()
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else::
+        if location == 0: # inserting to the beginning of the linked list
+            newNode.next = self.head
+            self.head = newNode
+        elif location == 1: # inserting to the end of the linked list
+            newNode.next = Node()
+            self.tail.next = newNode
+            self.tail = newNode
+        else: # inserting to in the middle
+            tempNode = self.head
+            index = 0
+            while index < location - 1:
+                tempNode = tempNode.next
+                index += 1
+            nextNode = tempNode.next
+            tempNode.next = newNode
+            newNode.next = nextNode
+
+
 
 
 singlyLinkedList = SLinkedList()
