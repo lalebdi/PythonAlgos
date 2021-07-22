72583d9d5789aafd2b59295d8361e9c6e29d909d
lalebdi <loai.alebdi@gmail.com>
2021-07-22 14:26:43 -0400

    debug

--- merge_2_sorted_linkedlists.py
+++ merge_2_sorted_linkedlists.py
@@ -85,12 +85,55 @@ class LinkedList:
             return 0
         return 1 + self.len_recursive(node.next)
 
+    def merge_sorted(self, llist):
+
+        p = self.head
+        q = llist.head
+        s = None
+
+        if not p:
+            return q
+        if not q:
+            return p
+
+        if p and q:
+            if p.data <= q.data:
+                s = p
+                p = s.next
+            else:
+                s = q
+                q = s.next
+            new_head = s
+        while p and q:
+            if p.data <= q.data:
+                s.next = p
+                s = p
+                p = s.next
+            else:
+                s.next = q
+                s = q
+                q = s.next
+        if not p:
+            s.next = q
+        if not q:
+            s.next = p
+        return new_head
 
 
-llist = LinkedList()
-llist.append("A")
-llist.append("B")
-llist.append("C")
-llist.append("D")
-llist.print_list()
-print(llist.len_iterative())
+llist_1 = LinkedList()
+llist_2 = LinkedList()
+
+llist_1.append(1)
+llist_1.append(5)
+llist_1.append(7)
+llist_1.append(9)
+llist_1.append(10)
+
+llist_2.append(2)
+llist_2.append(3)
+llist_2.append(4)
+llist_2.append(6)
+llist_2.append(8)
+
+llist_1.merge_sorted(llist_2)
+llist_1.print_list()
