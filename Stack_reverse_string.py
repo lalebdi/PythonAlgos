1644d92b062ec74ca0c5ff1745e5e4e2186cc31b
lalebdi <loai.alebdi@gmail.com>
2021-07-22 10:53:30 -0400

    init

---
+++ Stack_reverse_string.py
@@ -0,0 +1,24 @@
+""""
+
+"""
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, value):
+        self.items.append(value)
+
+    def pop(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return self.items == []
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def get_stack(self):
+        return self.items
+
