76b5f436deee0dcf583c0fb3c3ddce322f361847
lalebdi <loai.alebdi@gmail.com>
2021-07-27 11:46:18 -0400

    debug

--- LinkedLists_Remove_duplicates.py
+++ LinkedLists_Remove_duplicates.py
@@ -80,7 +80,7 @@ class LinkedList:
     def len_iterative(self):
         count = 0
         cur_node = self.head
-        while cur_node.next:
+        while cur_node:
             count += 1
             cur_node = cur_node.next
         return count
