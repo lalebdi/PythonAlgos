e365a484e42e428e3f8364d63fbe64761b9250e3
lalebdi <loai.alebdi@gmail.com>
2021-07-27 20:44:37 -0400

    init

---
+++ Circular_linkedlist.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def prepend(self, data):
+        pass
+
+    def append(self, data):
+        pass
+
+    def print_list(self):
+        pass
