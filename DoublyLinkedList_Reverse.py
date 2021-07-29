ec5a4aa1149ceff8f4cd8f0bbeaffa9d858c5f5e
lalebdi <loai.alebdi@gmail.com>
2021-07-28 21:11:01 -0400

    init

---
+++ DoublyLinkedList_Reverse.py
@@ -0,0 +1,110 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
+
+    def append(self, data):
+        if self.head is None:
+            new_node = Node(data)
+            self.head = new_node
+            new_node.next = None
+            new_node.prev = None
+        else:
+            new_node = Node(data)
+            last_node = self.head
+            while last_node.next:
+                last_node = last_node.next
+            last_node.next = new_node
+            new_node.prev = last_node
+            new_node.next = None
+
+    def prepend(self, data):
+        if self.head is None:
+            new_node = Node(data)
+            self.head = new_node
+            new_node.prev = None
+        else:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+            new_node.prev = None
+
+    def add_after_node(self, key, data):
+        cur_node = self.head
+        while cur_node:
+            if cur_node.next is None and cur_node.data == key:
+                self.append(data)
+                return
+            elif cur_node.data == key:
+                new_node = Node(data)
+                nxt_node = cur_node.next
+                new_node.next = nxt_node
+                nxt_node.prev = new_node
+                cur_node.next = new_node
+                new_node.prev = cur_node
+            cur_node = cur_node.next
+
+    def add_before_node(self, key, data):
+        cur_node = self.head
+        while cur_node:
+            if cur_node.prev is None and cur_node.data == key:
+                self.prepend(data)
+            elif cur_node.data == key:
+                new_node = Node(data)
+                prev_node = cur_node.prev
+                prev_node.next = new_node
+                new_node.prev = prev_node
+                new_node.next = cur_node
+                cur_node.prev = new_node
+            cur_node = cur_node.next
+
+    def delete_node(self, key):
+        cur_node = self.head
+        while cur_node:
+            if cur_node.data == key and cur_node == self.head:
+                if not cur_node.next:
+                    cur_node = None
+                    self.head = None
+                    return
+                else:
+                    nxt_node = cur_node.next
+                    self.head = nxt_node
+                    nxt_node.prev = None
+                    cur_node.next = None
+                    cur_node = None
+                    return
+            elif cur_node.data == key:
+                if cur_node.next:
+                    nxt_node = cur_node.next
+                    prev_node = cur_node.prev
+                    prev_node.next = nxt_node
+                    nxt_node.prev = prev_node
+                    cur_node.next = None
+                    cur_node.prev = None
+                    cur_node = None
+                else:
+                    prev_node = cur_node.prev
+                    prev_node.next = None
+                    cur_node.prev = None
+                    cur_node = None
+                    return
+            cur_node = cur_node.next
+
+
+
+
+
+
