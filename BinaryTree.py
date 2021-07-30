e08c71c4b0ff00b0bf6822d0afd10f92729a3d54
lalebdi <loai.alebdi@gmail.com>
2021-07-29 20:37:32 -0400

    pre-order print

--- BinaryTree.py
+++ BinaryTree.py
@@ -8,3 +8,30 @@ class Node:
 class BinaryTree:
     def __init__(self, root):
         self.root = Node(root)
+
+    def preorder_print(self, start, traversal):
+        """
+        root -> left -> right
+        start: the node that will be updated on every recursive call of this function.
+        traversal: This a string that's going to be printed on the console and we going to be updating it on every
+        recursive call and it is the return value.
+        """
+        if start:
+            traversal += (str(start.value) + "-")
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
+        return traversal
+
+
+
+
+
+
+
+"""
+Pre-Order Traversal:
+1. Check if the current node is empty.
+2. Display the data part of the root, or the current node.
+3. Traverse the left subtree by recursively calling the pre-order function.
+4. Traverse the right subtree by recursively calling the pre-order function. 
+"""
