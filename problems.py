771b320bd1c5b8ee5de18a7c66e959bf48879a36
lalebdi <loai.alebdi@gmail.com>
2021-08-02 10:44:37 -0400

    permutation

--- problems.py
+++ problems.py
@@ -19,12 +19,29 @@ def unique_char(word):
     return len(unique) == 0
 
 
-print(unique_char("unique"))
-print(unique_char("bear"))
+# print(unique_char("unique"))
+# print(unique_char("bear"))
 
 
 def is_unique(s):
     return len(set(s)) == len(s)
 
-print(is_unique("unique"))
-print(is_unique("bear"))
+# print(is_unique("unique"))
+# print(is_unique("bear"))
+
+
+"""
+Problem:
+  Given two strings, write a function to decide if one is a permutation of the other. 
+"""
+
+
+def permutation(s1, s2):
+    arr1 = list(s1)
+    arr2 = list(s2)
+    return sorted(arr2) == sorted(arr1)
+
+
+print(permutation("driving", "drivign"))
+print(permutation("driving", "ddriving"))
+print(permutation("the cow jumps over the moon.", "the moon jumps over the cow."))
