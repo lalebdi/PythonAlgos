36d4e4b568738f8c4c508e6d0b012efc09640183
lalebdi <loai.alebdi@gmail.com>
2021-08-06 13:01:40 -0400

    done

--- palindrome.py
+++ palindrome.py
@@ -16,3 +16,22 @@ def is_palindrome(input_str):
 
 print(is_palindrome("level"))
 print(is_palindrome(s))
+
+
+def is_palindrome2(input_str):
+    i = 0
+    j = len(input_str) - 1
+    while i < j:
+        while not input_str[i].isalnum() and i < j:
+            i += 1
+        while not input_str[j].isalnum() and i < j:
+            j -= 1
+        if input_str[i].lower() != input_str[j].lower():
+            return False
+        i += 1
+        j -= 1
+    return True
+
+
+print(is_palindrome2("level"))
+print(is_palindrome2(s))
