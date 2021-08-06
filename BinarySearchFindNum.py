5fac75b4155e987d996fc15421fe58922374c6c2
lalebdi <loai.alebdi@gmail.com>
2021-08-06 11:18:27 -0400

    Recursive binary search

--- BinarySearchFindNum.py
+++ BinarySearchFindNum.py
@@ -30,3 +30,19 @@ def binary_search_iterative(data, target):
 
 
 print(binary_search_iterative(data, target))
+
+# Recursive Binary Search:
+def binary_search_recursive(data, target, low, high):
+    if low > high:
+        return False
+    else:
+        mid = (low + high) // 2
+        if target == data[mid]:
+            return True
+        elif target < data[mid]:
+            return binary_search_recursive(data, target, low, mid - 1)
+        else:
+            return binary_search_recursive(data, target, mid + 1, high)
+
+
+print(binary_search_recursive(data, target, 0, len(data) - 1))
