1cab6aa65dfa9ad3c9135f7b88269f52f46c6290
lalebdi <loai.alebdi@gmail.com>
2021-08-06 13:19:46 -0400

    done

--- is_anagram.py
+++ is_anagram.py
@@ -33,4 +33,42 @@ def is_anagram(str1, str2):
 print(is_anagram(s1, s2))
 print(is_anagram(s3, s4))
 
+# Requires O(n log n)
+def is_anagram2(str1, str2):
+    str1 = str1.replace(" ", "").lower()
+    str2 = str2.replace(" ", "").lower()
+    return sorted(str1) == sorted(str2)
 
+
+print(is_anagram2(s1, s2))
+print(is_anagram2(s3, s4))
+
+
+def is_anagram3(str1, str2):
+    ht = dict()
+    str1 = str1.replace(" ", "").lower()
+    str2 = str2.replace(" ", "").lower()
+
+    if len(str1) != len(str2):
+        return False
+
+    for i in str1:
+        if i in ht:
+            ht[i] += 1
+        else:
+            ht[i] = 1
+
+    for i in str2:
+        if i in ht:
+            ht[i] -= 1
+        else:
+            ht[i] = 1
+
+    for i in ht:
+        if ht[i] != 0:
+            return False
+    return True
+
+
+print(is_anagram3(s1, s2))
+print(is_anagram3(s3, s4))
