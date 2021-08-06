f57ff02a6f42d61aa3ca9176a831b02ef7eb732a
lalebdi <loai.alebdi@gmail.com>
2021-08-06 11:31:35 -0400

    recursive

--- Recursion-Find_Uppercase_Letter_in_String.py
+++ Recursion-Find_Uppercase_Letter_in_String.py
@@ -12,6 +12,9 @@ str_3 = "lucidprogramming"
 The algorithm should return "P", "L", and output a message indicating no such capital letter found,
 respectively for the above strings.
 """
+str_1 = "lucidProgramming"
+str_2 = "LucidProgramming"
+str_3 = "lucidprogramming"
 
 
 def find_upper(string):
@@ -21,6 +24,20 @@ def find_upper(string):
     return False
 
 
-print(find_upper("lucidProgramming"))
-print(find_upper("LucidProgramming"))
-print(find_upper("lucidprogramming"))
+print(find_upper(str_1))
+print(find_upper(str_2))
+print(find_upper(str_3))
+
+
+
+def find_uppercase_recursive(input_str, idx = 0):
+    if input_str[idx].isupper():
+        return input_str[idx]
+    if idx == len(input_str) - 1:
+        return False
+    return find_uppercase_recursive(input_str, idx + 1)
+
+
+print(find_uppercase_recursive(str_1))
+print(find_uppercase_recursive(str_2))
+print(find_uppercase_recursive(str_3))
