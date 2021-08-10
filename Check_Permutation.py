ed3320fc1751fbf7161e6f63b8112a342f78f68d
lalebdi <loai.alebdi@gmail.com>
2021-08-10 12:22:22 -0400

    solution 1

--- Check_Permutation.py
+++ Check_Permutation.py
@@ -37,5 +37,28 @@ def permuation_check(input_str1, input_str2):
             return False
     return True
 
+
 print(permuation_check(is_permutation_1, is_permutation_2))
 print(permuation_check(not_permutation_1, not_permutation_2))
+
+
+def is_perm1(str1, str2):
+    str1 = str1.lower()
+    str2 = str2.lower()
+
+    if len(str1) != len(str2):
+        return False
+
+    str1 = "".join(sorted(str1))
+    str2 = "".join(sorted(str2))
+
+    n = len(str1)
+
+    for i in range(n):
+        if str1[i] != str2[i]:
+            return False
+    return True
+
+
+print(is_perm1(is_permutation_1,is_permutation_2))
+print(is_perm1(not_permutation_1, not_permutation_2))
