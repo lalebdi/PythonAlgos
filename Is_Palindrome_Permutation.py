bf598a43fba5e3af32edd9e1eeba591e63608d3b
lalebdi <loai.alebdi@gmail.com>
2021-08-09 22:48:31 -0400

    solution 1

--- Is_Palindrome_Permutation.py
+++ Is_Palindrome_Permutation.py
@@ -8,3 +8,29 @@ Palindrome: A word or phrase that is the same forwards and backward.
 
 Permutation: A rearrangement of letters.The palindrome does not need to be limited to just dictionary words.
 """
+import string
+
+palin_perm = "Tact Coa" # Taco Cat
+not_palin_perm = "This is not a palindrome permutation"
+
+
+def is_perm_palin(input_str):
+    word = input_str.replace(" ", "").lower()
+    word = word.translate(str.maketrans("", "", string.punctuation))
+    chars = {}
+    for i in word:
+        if i in chars:
+            chars[i] += 1
+        else:
+            chars[i] = 1
+
+    unique_chars = 0
+    for i in chars.values():
+        if i % 2 != 0:
+            unique_chars += 1
+
+    return True if unique_chars == 1 else False
+
+
+print(is_perm_palin(palin_perm))
+print(is_perm_palin(not_palin_perm))
