6132a23fb85912b7ca3de8cde2200cfa6c592e85
lalebdi <loai.alebdi@gmail.com>
2021-08-09 22:32:49 -0400

    init

---
+++ Is_Palindrome_Permutation.py
@@ -0,0 +1,10 @@
+"""
+Problem:
+  Given a string, we wish to write a function to determine if it is a permutation of a palindrome.
+
+Recall:
+
+Palindrome: A word or phrase that is the same forwards and backward.
+
+Permutation: A rearrangement of letters.The palindrome does not need to be limited to just dictionary words.
+"""
