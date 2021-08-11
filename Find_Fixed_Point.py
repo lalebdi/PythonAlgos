b5ea2eb105bc43b8c74ad4af380262f04a5fc5a2
lalebdi <loai.alebdi@gmail.com>
2021-08-11 13:50:39 -0400

    log n solution

--- Find_Fixed_Point.py
+++ Find_Fixed_Point.py
@@ -15,13 +15,41 @@ arr2 = [0, 2, 5, 8, 17]
 # No fixed point. Return None:
 arr3 = [-10, -5, 3, 4, 7, 9]
 
-
+# Time complexity O(n)
+# Space complexity O(1)
 def fixed_point_1(arr):
     for i, v in enumerate(arr):
         if i == v:
             return v
     return None
 
+
 print(fixed_point_1(arr1))
 print(fixed_point_1(arr2))
 print(fixed_point_1(arr3))
+
+# Time complexit O(log n)
+# Space complexity O(1)
+def find_fixed_point(arr):
+    high = len(arr) - 1
+    low = 0
+
+    if len(arr) == 0:
+        return None
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] < mid:
+            low = mid + 1
+        elif arr[mid] > mid:
+            high = mid - 1
+        else:
+           return mid
+
+    return None
+
+
+print(find_fixed_point(arr1))
+print(find_fixed_point(arr2))
+print(find_fixed_point(arr3))
