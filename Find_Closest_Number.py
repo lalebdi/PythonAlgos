0a2862b3ac366c157c3ca9bf07618b076d9db2fd
lalebdi <loai.alebdi@gmail.com>
2021-08-11 12:36:48 -0400

    init

---
+++ Find_Closest_Number.py
@@ -0,0 +1,25 @@
+"""
+Problem:
+ Given a sorted array and a target number. Our goal is to find a number in the array that is closest to the target number.
+  We will be making use of binary search to solve this problem.
+
+The array may contain duplicate values and negative numbers.
+
+Example 1:
+    Input : arr[] = {1, 2, 4, 5, 6, 6, 8, 9}
+    Target number = 11
+    Output : 9
+    9 is closest to 11 in given array
+
+Example 2:
+    Input :arr[] = {2, 5, 6, 7, 8, 8, 9};
+    Target number = 4
+    Output : 5
+"""
+
+arr1 = [1, 2, 4, 5, 6, 6, 8, 9]
+target_number1 = 11
+
+arr2 = [2, 5, 6, 7, 8, 8, 9]
+target_number2 = 4
+
