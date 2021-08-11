b8074dac493351d0abc741a48fa699d602c7984d
lalebdi <loai.alebdi@gmail.com>
2021-08-10 20:44:25 -0400

    init

---
+++ Is_Unique.py
@@ -0,0 +1,32 @@
+"""
+Problem:
+  Given a string, determine if a string has all unique characters.
+"""
+import string
+
+unique_str = "AbCDefG"
+non_unique_str = "non Unique STR"
+
+
+def is_unique1(input_str):
+    input_str = input_str.replace(" ", "").lower()
+    input_str = input_str.translate(str.maketrans("", "", string.punctuation))
+
+    letters = dict()
+
+    for i in input_str:
+        if i in letters:
+            letters[i] += 1
+        else:
+            letters[i] = 1
+
+    for i in letters.values():
+        if i > 1:
+            return False
+    return True
+
+
+print(is_unique1(unique_str))
+print(is_unique1(non_unique_str))
+
+
