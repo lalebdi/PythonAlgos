a4a9d114a22d8d5264f9b506e659613f91ae054a
lalebdi <loai.alebdi@gmail.com>
2021-08-11 15:03:34 -0400

    init

---
+++ Find_First_Entry_in_List_with_Duplicates.py
@@ -0,0 +1,13 @@
+"""
+Problem:
+ write a function that takes an array of sorted integers and a key and returns the index of the first occurrence of that
+  key from the array.
+
+For example, for the array:
+[-14, -10, 2, 108, 108, 243, 285, 285, 285, 401]
+
+with target = 108, the algorithm would return 3, as the first occurrence of 108 in the above array is located at index 3
+"""
+
+arr = [-14, -10, 2, 108, 108, 243, 285, 285, 285, 401]
+target = 108
