5432144ca915e3690740c14adf209598ceac0afa
lalebdi <loai.alebdi@gmail.com>
2021-08-11 14:31:28 -0400

    none

---
+++ Find_Bitonic_Peak.py
@@ -0,0 +1,16 @@
+"""
+Problem:
+ Given an array that is bitonically sorted,
+  an array that starts off with increasing terms and then concludes with decreasing terms.
+   In any such sequence, there is a "peak" element, that is, the element in the sequence that is the largest element.
+     find the peak element of a bitonic sequence.
+"""
+
+# Peak element is 5
+arr1 = [1, 2, 3, 4, 5, 4, 3, 2, 1]
+
+# Peak element is 4
+arr2 = [1, 2, 3, 4, 1]
+
+# Peak element is 6
+arr3 = [1, 6, 5, 4, 3, 2, 1]
