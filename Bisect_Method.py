5d3e3056379a1b47bba0b72fe9a895e90947b11c
lalebdi <loai.alebdi@gmail.com>
2021-08-12 11:10:27 -0400

    solution withou using bisect

--- Bisect_Method.py
+++ Bisect_Method.py
@@ -10,3 +10,27 @@ with target = 108, the algorithm would return 3, as the first occurrence of 108 
 """
 
 arr = [-14, -10, 2, 108, 108, 243, 285, 285, 285, 401]
+
+
+def first_occurence(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] < target:
+            low = mid + 1
+        elif arr[mid] > target:
+            high = mid - 1
+        else:
+            if mid - 1 < 0:
+                return mid
+            if arr[mid - 1] != target:
+                return mid
+            high = mid - 1
+
+    return None
+
+
+print(first_occurence(arr, 108))
