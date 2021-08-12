2078bfb29c2f66b30984a8e2a22dcf9e4b41fa2d
lalebdi <loai.alebdi@gmail.com>
2021-08-12 12:00:52 -0400

    2 soltuions

--- Cyclically_Shifted_Array.py
+++ Cyclically_Shifted_Array.py
@@ -11,3 +11,20 @@ of the cyclically sorted array.
 """
 
 ARR = [4, 5, 6, 7, 1, 2, 3]
+
+
+def find_index_of_lowest1(arr):
+    lowest = min(arr)
+    return arr.index(lowest)
+
+
+print(find_index_of_lowest1(ARR))
+
+
+def find_index_of_lowest2(arr):
+    lowest = min(arr)
+    for i, v in enumerate(arr):
+        if v == lowest:
+            return i
+
+print(find_index_of_lowest2(ARR))
