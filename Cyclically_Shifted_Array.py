1d10023e061bfc0e508d0ee0e36f643d2fb3396b
lalebdi <loai.alebdi@gmail.com>
2021-08-12 11:58:06 -0400

    init

---
+++ Cyclically_Shifted_Array.py
@@ -0,0 +1,13 @@
+"""
+Problem:
+ An array is "cyclically sorted" if it is possible to cyclically shift
+its entries so that it becomes sorted.
+The following list is an example of a cyclically sorted array:
+
+    A = [4, 5, 6, 7, 1, 2, 3]
+
+Write a function that determines the index of the smallest element
+of the cyclically sorted array.
+"""
+
+ARR = [4, 5, 6, 7, 1, 2, 3]
