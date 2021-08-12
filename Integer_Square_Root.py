715313f7dd2a1de1ecbf6c77216102e39df2db7f
lalebdi <loai.alebdi@gmail.com>
2021-08-12 11:36:58 -0400

    init

---
+++ Integer_Square_Root.py
@@ -0,0 +1,22 @@
+"""
+Problem:
+ write a function that computes the integer square root of a given number as input
+  without using any built-in square root function.
+
+Specifically, write a function that takes a non-negative integer and returns the largest integer whose square is less
+ than or equal to the integer given:
+
+Example:
+
+Assume input is integer 300.
+
+Then the expected output of the function should be 17 since 17 squared is 289 which is strictly less than 300.
+ Note that 18 squared is 324 which is strictly greater than 300, so the number 17 is the correct response.
+"""
+
+
+def square_root(num):
+    return int(num ** 0.5)
+
+
+print(square_root(300))
