c2c91165dbfb0ec59365ff0bafed134cf772d66b
lalebdi <loai.alebdi@gmail.com>
2021-08-13 12:53:51 -0400

    solution 2

--- String_to_Integer.py
+++ String_to_Integer.py
@@ -27,5 +27,30 @@ def str_to_int1(input_str):
 
     return output_int
 
+
 print(str_to_int1("123"))
 print(str_to_int1("-123"))
+
+
+def str_to_int2(input_str):
+    output_int = 0
+
+    if input_str[0] == "-":
+        is_negative = True
+        input_str = input_str[1:]
+    else:
+        is_negative = False
+
+    for i in range(len(input_str)):
+        place = 10 ** (len(input_str) - (i + 1))
+        digit = place * (ord(input_str[i]) - ord("0"))
+        output_int += digit
+
+    if is_negative:
+        output_int *= -1
+
+    return output_int
+
+
+print(str_to_int2("123"))
+print(str_to_int2("-123"))
