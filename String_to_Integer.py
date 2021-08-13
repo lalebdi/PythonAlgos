a69044a5c9aab5c23bb333317ada16687f993060
lalebdi <loai.alebdi@gmail.com>
2021-08-13 12:25:16 -0400

    init

---
+++ String_to_Integer.py
@@ -0,0 +1,11 @@
+"""
+Problem:
+  You are given some numeric string as input. Convert the string you are
+given to an integer. Do not make use of the built-in "int" function.
+Example:
+    "123" : 123
+    "-12332" : -12332
+    "554" : 554
+    etc.
+"""
+
