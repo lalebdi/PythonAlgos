0f25599c5ee23f9d1542db93f0e8b1782a1beeab
lalebdi <loai.alebdi@gmail.com>
2021-08-13 10:15:36 -0400

    init

---
+++ Integer_to_String.py
@@ -0,0 +1,11 @@
+"""
+Problem:
+  You are given some integer as input, (i.e. ... -3, -2, -1, 0, 1, 2, 3 ...)
+Convert the integer you are given to a string. Do not make use
+of the built-in "str" function.
+Examples:
+    Input: 123
+    Output: "123"
+    Input: -123
+    Output: "-123"
+"""
