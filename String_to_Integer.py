9977047052ec2f8ebe1ab08dca970681c0476c36
lalebdi <loai.alebdi@gmail.com>
2021-08-13 12:25:38 -0400

    solution 1

--- String_to_Integer.py
+++ String_to_Integer.py
@@ -9,3 +9,23 @@ Example:
     etc.
 """
 
+
+def str_to_int1(input_str):
+    if input_str[0] == "-":
+        is_negative = True
+        input_str = input_str[1:]
+    else:
+        is_negative = False
+
+    output_int = 0
+
+    for i in input_str:
+        output_int = output_int * 10 + ord(i) - ord("0")
+
+    if is_negative:
+        output_int *= -1
+
+    return output_int
+
+print(str_to_int1("123"))
+print(str_to_int1("-123"))
