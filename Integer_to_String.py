00b95a1df7048b826aa03f9aa19dba91ebe97358
lalebdi <loai.alebdi@gmail.com>
2021-08-13 10:48:29 -0400

    produce result

--- Integer_to_String.py
+++ Integer_to_String.py
@@ -9,3 +9,22 @@ Examples:
     Input: -123
     Output: "-123"
 """
+
+
+def int_to_str(input_int):
+    if input_int < 0:
+        is_negative = True
+        input_int *= -1
+    else:
+        is_negative = True
+
+    output_str = []
+    while input_int > 0:
+        # print(input_int % 10)
+        output_str.append(chr(ord("0") + input_int % 10))
+        input_int //= 10
+
+
+    return "".join(output_str[::-1])
+
+print(int_to_str(123))
