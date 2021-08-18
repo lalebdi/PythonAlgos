313eb58f624b4d3438ae1dafb6cfef73696ccd65
lalebdi <loai.alebdi@gmail.com>
2021-08-18 11:14:24 -0400

    is and ==

--- guess_outputs.py
+++ guess_outputs.py
@@ -16,15 +16,22 @@ print(walrus)
 
 
 """
-String Intern
+String Intern => Mutability of Objects
+Intern: A method of storing only one copy of each distinct string value, which must be immutable.
 """
 
 a = "".join(['o', 'k', 'a', 'y'])
 
-b = 'o' + 'k' + 'a' + 'y'
+b = 'o' + 'k' + 'a' + 'y' # b and c are pointing to the same object
 
-c = "okay"
+c = "okay" # b and c are pointing to the same object
 
-print(a is b)
+print(a is b) # the interpreter doesn't know what will the value of a is going to be. Thus, stored as a separate object.
 print(a is c)
 print(b is c)
+"""
+'is' is not the same as '==' . '==' compares the equivalence of objects. 'is' checks the id of the objects, same objects
+"""
+print(a == b)
+print(a == c)
+print(b == c)
