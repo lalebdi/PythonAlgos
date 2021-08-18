920db923e7de34287ca18aa86f778c2d6b2dc9ff
lalebdi <loai.alebdi@gmail.com>
2021-08-18 11:04:07 -0400

    String Intern

--- guess_outputs.py
+++ guess_outputs.py
@@ -13,3 +13,18 @@ if (x := func(4)) >= 5:
 
 (walrus := func(2))
 print(walrus)
+
+
+"""
+String Intern
+"""
+
+a = "".join(['o', 'k', 'a', 'y'])
+
+b = 'o' + 'k' + 'a' + 'y'
+
+c = "okay"
+
+print(a is b)
+print(a is c)
+print(b is c)
