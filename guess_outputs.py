afc57b10b9654590596109a34b02615281bbb9b7
lalebdi <loai.alebdi@gmail.com>
2021-08-18 11:37:04 -0400

    all() function

--- guess_outputs.py
+++ guess_outputs.py
@@ -41,13 +41,13 @@ Chained Operations
 1 AND True are the same
 """
 
-print((1 == 1) in [1])
-
-print(1 == (1 in [1]))
-
-print(1 < (0 < 1))
-
-print(1 < 0 < 1) # this valued as =>  1 < 0 and 0 < 1
+# print((1 == 1) in [1])
+#
+# print(1 == (1 in [1]))
+#
+# print(1 < (0 < 1))
+#
+# print(1 < 0 < 1) # this valued as =>  1 < 0 and 0 < 1
 
 """
 Dictionary Key Hashing
@@ -58,4 +58,20 @@ d[1] = "hi"
 d[1.0] = "hello"
 d[2 - 1] = "yo"
 
-print(d)
+# print(d)
+# x = hash(1)
+# y = hash(2 - 1)
+# z = hash(1.0)
+# print(x == y)
+# print(x == z)
+
+"""
+all() function:
+    Takes an iterable object and tells me if all the values in it are equal to True
+"""
+
+print(all([1, 2, 3, False]))
+print(all([1, 2, 3]))
+# an empty nested list == False
+print(all([[]]))
+print(all([[1]]))
