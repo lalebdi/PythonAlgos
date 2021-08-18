7bcb024d7936f6993565fd8bef9dcdea22fc8469
lalebdi <loai.alebdi@gmail.com>
2021-08-18 11:20:04 -0400

    chained operations

--- guess_outputs.py
+++ guess_outputs.py
@@ -12,7 +12,7 @@ if (x := func(4)) >= 5:
 
 
 (walrus := func(2))
-print(walrus)
+# print(walrus)
 
 
 """
@@ -26,12 +26,24 @@ b = 'o' + 'k' + 'a' + 'y' # b and c are pointing to the same object
 
 c = "okay" # b and c are pointing to the same object
 
-print(a is b) # the interpreter doesn't know what will the value of a is going to be. Thus, stored as a separate object.
-print(a is c)
-print(b is c)
+# print(a is b) # the interpreter doesn't know what will the value of a is going to be. Thus, stored as a separate object.
+# print(a is c)
+# print(b is c)
 """
 'is' is not the same as '==' . '==' compares the equivalence of objects. 'is' checks the id of the objects, same objects
 """
-print(a == b)
-print(a == c)
-print(b == c)
+# print(a == b)
+# print(a == c)
+# print(b == c)
+
+"""
+chained operations
+"""
+
+print((1 == 1) in [1])
+
+print(1 == (1 in [1]))
+
+print(1 < (0 < 1))
+
+print(1 < 0 < 1)
