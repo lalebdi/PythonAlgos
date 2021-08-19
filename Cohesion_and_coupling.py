c5d3bd416dd7ccdfbfa76986bd0848c9c4590e17
lalebdi <loai.alebdi@gmail.com>
2021-08-19 11:24:17 -0400

    init

---
+++ Cohesion_and_coupling.py
@@ -0,0 +1,6 @@
+"""
+Cohesion and coupling:
+    High level concept. THey say how your code is easily changed or extended.
+Cohesion: Degree to which elements of a certain class belong together. (Has a clear responsibility).
+Coupling: is a measure of how dependent 2 parts of your code together. The more coupling the more difficult to maintain.
+"""
